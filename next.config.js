1fea6fef4f094b39f6aa9ecabed6f38533208037
Szőke Márk <szoke.mark909@gmail.com>
2023-09-28 07:12:45 +0200

    config: nextjs img optimization + cache

--- next.config.js
+++ next.config.js
@@ -17,6 +17,10 @@ const nextConfig = {
   },
   images: {
     domains: ['randomuser.me'],
+    minimumCacheTTL: 60,
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 };
 

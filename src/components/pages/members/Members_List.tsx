29f61a097aa245ce6e88e470a930ada194bfde31
Szőke Márk <szoke.mark909@gmail.com>
2023-09-28 11:29:15 +0200

    feat: infinite scrolling, the limit resets if any of the filters changed + no item found card

--- src/components/pages/members/Members_List.tsx
+++ src/components/pages/members/Members_List.tsx
@@ -1,27 +1,53 @@
 import MemberCard from '@@components/MemberCard';
 import { MembersGrid } from '@@components/pages/members/Members.styles';
 import useMembers from '@@contexts/MembersCtx';
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+/**
+ * Constants
+ */
+const maxMembersStep = 12;
 
 /**
  * Component: Members_List
  */
 export default function Members_List() {
-  const { members, filteredMembers, loading } = useMembers();
+  const ref = useRef<HTMLElement>(null);
+  const { members, filteredMembers, filter, loading } = useMembers();
+  const [MaxMember, setMaxMember] = useState(maxMembersStep);
+
+  const fn = useCallback(() => {
+    if (!ref.current) return;
+    const current = window.scrollY + window.innerHeight - 150;
+    const limit = ref.current.offsetTop + ref.current.offsetHeight;
+    if (current >= limit) setMaxMember((v) => v + maxMembersStep);
+  }, []);
+
+  useEffect(() => setMaxMember(maxMembersStep), [filter.name, filter.state.length, filter.sort]);
+  useEffect(() => {
+    window.addEventListener('scroll', fn);
+    return () => window.removeEventListener('scroll', fn);
+  }, [fn]);
 
   if (loading && !members.length)
     return (
       <MembersGrid>
-        {Array.from({ length: 12 }, (_, i) => (
+        {Array.from({ length: maxMembersStep }, (_, i) => (
           <MemberCard loading key={i} />
         ))}
       </MembersGrid>
     );
 
   return (
-    <MembersGrid>
-      {filteredMembers.map((member, i) => (
+    <MembersGrid ref={ref}>
+      {filteredMembers.slice(0, MaxMember).map((member, i) => (
         <MemberCard data={member} key={i} />
       ))}
+      {!loading && !filteredMembers.length && (
+        <div className="NoItemFound">
+          <i>⚠️</i>No item found<i>⚠️</i>
+        </div>
+      )}
     </MembersGrid>
   );
 }
